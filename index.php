bd1e8772a0199c1f78f29a3344f47e2f9fa115fd
iijuh3016 <ahmadjuhdi64@gmail.com>
2025-03-10 13:07:34 +0800

    membuat file prodi.php

--- index.php
+++ index.php
@@ -6,6 +6,26 @@
     <title>SIMPADU</title>
 </head>
 <body>
-    <h1>DATA MAHASISWA</h1
+    <h1>DATA MAHASISWA</h1>
+    <table border="1" cellspasing="0" cellpadding="5" >
+        <thead>
+                <th>No</th>
+                <th>NIM</th>
+                <th>Nama</th>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>E020323084</td>
+                <td>ahmad juhdi</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>E020323072</td>
+                <td>isan</td>
+            </tr>
+        </tbody>
+    </table>
+
 </body>
 </html>
